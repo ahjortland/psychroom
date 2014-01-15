a0ae3c7d7dc6b1cce0435cfc8b1d2ef8ae1e7d17
Andrew Hjortland <hjortlanda@gmail.com>
2014-01-15 10:51:56 -0500

    Removed some methods in the tools module that were not needed anymore.

--- psychroom/tools.py
+++ psychroom/tools.py
@@ -1,7 +1,5 @@
 """Useful tools when dealing with Test Data Frames."""
 
-from unit import Unit
-
 
 def _get_metadata(self, attr, **kwargs):
     """Get a metadata property from a data frame.
@@ -99,47 +97,3 @@ def descriptions(self, keys=None):
         return {key: val for key, val in desc.items() if key in keys}
     else:
         return desc
-
-
-def _unit(series):
-    """Return the unit of a series if defined, else return undefined.
-
-    Parameters
-    ----------
-    series : pandas Series
-
-    Returns
-    -------
-    unit : unit.Unit
-        the unit type of the series or an undefined Unit if it is
-        undefined.
-
-    """
-    try:
-        return series.unit
-    except AttributeError:
-        return Unit('undefined')
-
-
-def update_units(frame):
-    """Update the units attribute of a test data frame.
-
-    Parameters
-    ----------
-    frame : pandas DataFrame
-        experimental test data frame
-
-    Returns
-    -------
-    result : pandas DataFrame
-        copy of the original experimental test data frame with an
-        updated (or added) units attribute.
-
-    """
-
-    result = frame.copy(deep=True)
-    result.units = get_units(frame)
-    for key, val in result.units.items():
-        result[key].unit = val
-
-    return result
